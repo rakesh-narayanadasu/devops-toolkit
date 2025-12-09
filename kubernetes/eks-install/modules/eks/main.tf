d024abfea621fb45f5e71a5d0447d0f73a01bd35
Rakesh Narayanadasu <126420803+rakesh-narayanadasu@users.noreply.github.com>
2025-12-09 19:48:47 +0530

    added metrics server

--- kubernetes/eks-install/modules/eks/main.tf
+++ kubernetes/eks-install/modules/eks/main.tf
@@ -178,3 +178,24 @@ resource "helm_release" "cluster_autoscaler" {
     awsRegion = "us-east-1"
   })]
 }
+
+# Metrics Server
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+
+  values = [
+    yamlencode({
+      args = [
+        "--kubelet-insecure-tls",
+        "--kubelet-preferred-address-types=InternalIP,Hostname,InternalDNS,ExternalDNS,ExternalIP"
+      ]
+    })
+  ]
+
+  depends_on = [aws_eks_node_group.main]
+}
