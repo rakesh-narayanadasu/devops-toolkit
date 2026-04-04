658efa30037991da743c0eb3e3d2458b0607e9ee
Rakesh Narayanadasu <126420803+rakesh-narayanadasu@users.noreply.github.com>
2026-04-04 21:48:52 +0530

    Create argocd.sh

---
+++ tools-installation/argocd.sh
@@ -0,0 +1,11 @@
+# ArgoCD
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl get pods -n argocd
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+kubectl -n argocd get secret argocd-initial-admin-secret \
+-o jsonpath="{.data.password}" | base64 -d && echo
